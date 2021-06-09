b5456e665108bd42ec287ad49e0680addf69c8cc
YeoCheal <63528884+YeoCheal@users.noreply.github.com>
2021-06-10 01:46:38 +0900

    Update Chapter_03_Exercise_02.py

--- Chapter_03_Exercise_02.py
+++ Chapter_03_Exercise_02.py
@@ -1,2 +1,26 @@
-# Exercise 2 : Rewrite your pay program using try and except so that your program handles non-numeric input gracefully by printing a message and exiting the program. The following shows two execitions of the program :
+# Exercise 2
+#
+# Rewrite your pay program using try and except
+# so that your program handles non-numeric input gracefully
+# by printing a message and exiting the program.
+# The following shows two execitions of the program :
 
+
+try :
+  hrs = input('Enter Hours : ')
+  rate = input('Enter Rate : ')
+
+  hrs = float(hrs)
+  rate = float(rate)
+
+  if hrs > 40 :
+    pay = ((hrs - 40) * rate * 1.5) + (40 * rate)
+    print(pay)
+  else :
+    pay = hrs * rate
+    print(pay)
+    
+except :
+  print('Error, please enter numeric input')
+  
+  
