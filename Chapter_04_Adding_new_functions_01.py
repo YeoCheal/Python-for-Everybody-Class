d1ff58fcf6cf5dbdb585e2ce062ab1741836b16b
YeoCheal <63528884+YeoCheal@users.noreply.github.com>
2021-06-17 15:38:54 +0900

    Create Chapter_04_Adding_new_functions_01.py

---
+++ Chapter_04_Adding_new_functions_01.py
@@ -0,0 +1,16 @@
+def print_lyrics():
+    print("I'm a lumberjerks, and I'm okay.")
+    print("I sleep all night and I work all day.")
+
+print_lyrics()
+
+print(print_lyrics)
+
+print(type(print_lyrics))
+
+
+def repeat_lyrics():
+  print_lyrics()
+  print_lyrics()
+  
+repeat_lyrics()
