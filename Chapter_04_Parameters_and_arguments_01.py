6dc626e3736eed841e654aafebac3d83d422ca12
YeoCheal <63528884+YeoCheal@users.noreply.github.com>
2021-06-18 17:26:48 +0900

    Create Chapter_04_Parameters_and_arguments_01.py

---
+++ Chapter_04_Parameters_and_arguments_01.py
@@ -0,0 +1,18 @@
+def print_twice(bruce):
+  print(bruce)
+  print(bruce)
+
+
+print_twice('Spam ')
+
+print_twice(17)
+
+import math
+print_twice(math.pi)
+
+print_twice('Spam ' * 6)
+
+print_twice(math.cos(math.pi))
+
+michael = 'Eric, We can build a new world.'
+print_twice(michael)
